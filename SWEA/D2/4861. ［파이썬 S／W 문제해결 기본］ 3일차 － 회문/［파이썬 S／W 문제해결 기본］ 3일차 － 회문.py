a4e7b2fea7d7ffcdd346e9d01db84e6b896b66e1
seoyoung671 <tjdud1202@gmail.com>
2025-08-10 19:34:27 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 3일차 - 회문, Time: 108 ms, Memory: 61,312 KB -BaekjoonHub

--- SWEA/D2/4861. ［파이썬 S／W 문제해결 기본］ 3일차 － 회문/［파이썬 S／W 문제해결 기본］ 3일차 － 회문.py
+++ SWEA/D2/4861. ［파이썬 S／W 문제해결 기본］ 3일차 － 회문/［파이썬 S／W 문제해결 기본］ 3일차 － 회문.py
@@ -3,24 +3,21 @@ T = int(input())
 for t in range(1,T+1):
     N,M = map(int,input().split())
     arr = [list(input()) for _ in range(N)]
-    # 탐색의 편의를 위해 각 알파벳을 요소로 갖는 리스트로 받음
 
-    # 가로의 경우
     for i in range(N):
         for j in range(N-M+1):
             tmp = ''
-            for k in range(j,M+j):
-                tmp = tmp + (arr[i][k])
+            for k in range(M):
+                tmp = tmp + arr[i][j+k]
             reverse = tmp[::-1]
             if tmp == reverse:
                 print(f"#{t} {tmp}")
 
-    # 세로의 경우
-    for i in range(N):
-        for j in range(N-M+1):
+    for j in range(N):
+        for i in range(N-M+1):
             tmp = ''
-            for k in range(j,M+j):
-                tmp = tmp + (arr[k][i])
+            for k in range(M):
+                tmp = tmp + arr[i+k][j]
             reverse = tmp[::-1]
             if tmp == reverse:
                 print(f"#{t} {tmp}")
