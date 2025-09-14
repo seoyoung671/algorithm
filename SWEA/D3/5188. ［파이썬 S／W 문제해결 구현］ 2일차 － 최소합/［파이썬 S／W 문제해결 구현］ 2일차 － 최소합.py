f82f1b24a2598fc0c4f7664c8a1f71d892358a94
seoyoung671 <tjdud1202@gmail.com>
2025-09-15 01:01:06 +0900

    [D3] Title: [파이썬 S/W 문제해결 구현] 2일차 - 최소합, Time: 309 ms, Memory: 70,016 KB -BaekjoonHub

--- SWEA/D3/5188. ［파이썬 S／W 문제해결 구현］ 2일차 － 최소합/［파이썬 S／W 문제해결 구현］ 2일차 － 최소합.py
+++ SWEA/D3/5188. ［파이썬 S／W 문제해결 구현］ 2일차 － 최소합/［파이썬 S／W 문제해결 구현］ 2일차 － 최소합.py
@@ -1,23 +1,27 @@
-def minsum(x,y,summation):
+
+def minsum(total,x,y):
     global result
-    if summation >= result:
+    if total >= result:
         return
 
     if x == N-1 and y == N-1:
-        result = min(result,summation)
+        result = total
         return
 
-    for dx,dy in (0,1),(1,0):
-        nx, ny = x+dx, y+dy
+    for i,j in (0,1),(1,0):
+        nx, ny = x +i, y+j
         if 0 <= nx < N and 0 <= ny < N:
-            minsum(nx,ny,summation + arr[nx][ny])
+            minsum(total+arr[nx][ny],nx,ny)
+
 
 
 T = int(input())
+
 for t in range(1,T+1):
     N = int(input())
     arr = [list(map(int,input().split())) for _ in range(N)]
 
-    result = 9999999999999999
-    minsum(0,0,arr[0][0])
+    result = 99999999999999
+
+    minsum(arr[0][0],0,0)
     print(f"#{t} {result}")
