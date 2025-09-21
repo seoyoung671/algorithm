db9f4dac0c05f92662f5335aea972650b80277c7
seoyoung671 <tjdud1202@gmail.com>
2025-09-21 20:00:20 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 72 ms, Memory: 59,776 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -1,15 +1,14 @@
-T = 10
-
 def bfs(si,sj):
     visited = [[0]*16 for _ in range(16)]
     q = [[si,sj]]
     visited[si][sj] = 1
+
     while q:
         ti,tj = q.pop(0)
         if arr[ti][tj] == 3:
             return 1
-        for di, dj in [[0,1],[1,0],[0,-1],[-1,0]]:
-            wi,wj = ti+di, tj+dj
+        for di,dj in [[0,1],[1,0],[0,-1],[-1,0]]:
+            wi,wj = ti + di, tj + dj
             if 0 <= wi < 16 and 0 <= wj < 16 and arr[wi][wj] != 1 and visited[wi][wj] == 0:
                 q.append([wi,wj])
                 visited[wi][wj] = visited[ti][tj] + 1
@@ -17,6 +16,8 @@ def bfs(si,sj):
         return 0
 
 
+
+T = 10
 for t in range(1,T+1):
     N = int(input())
     arr = [list(map(int,input())) for _ in range(16)]
@@ -24,11 +25,6 @@ for t in range(1,T+1):
     for i in range(16):
         for j in range(16):
             if arr[i][j] == 2:
-                si, sj = i, j
-
-    for i in range(16):
-        for j in range(16):
-            if arr[i][j] == 3:
-                ei, ej = i, j
+                si,sj = i, j
 
     print(f"#{t} {bfs(si,sj)}")
