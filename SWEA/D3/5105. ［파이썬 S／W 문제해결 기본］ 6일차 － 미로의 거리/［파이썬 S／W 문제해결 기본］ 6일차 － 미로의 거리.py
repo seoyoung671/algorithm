1fac776ed6284647bcbfabc1b3c439dc01a1135a
seoyoung671 <tjdud1202@gmail.com>
2025-09-29 00:24:00 +0900

    [D3] Title: [파이썬 S/W 문제해결 기본] 6일차 - 미로의 거리, Time: 80 ms, Memory: 59,904 KB -BaekjoonHub

--- SWEA/D3/5105. ［파이썬 S／W 문제해결 기본］ 6일차 － 미로의 거리/［파이썬 S／W 문제해결 기본］ 6일차 － 미로의 거리.py
+++ SWEA/D3/5105. ［파이썬 S／W 문제해결 기본］ 6일차 － 미로의 거리/［파이썬 S／W 문제해결 기본］ 6일차 － 미로의 거리.py
@@ -1,30 +1,43 @@
-def find_start(N):
-    for i in range(N):
-        for j in range(N):
-            if maze[i][j] == 2:
-                return i, j
-
-def bfs(i,j,N):
-    visited = [[0]*N for _ in range(N)]
-    q = [[i,j]]
-    visited[i][j] = 1 # 인큐표시. 준비작업 끝
-    while q:
-        ti,tj = q.pop(0) # 디큐
-        if maze[ti][tj] == 3: # 도착지인지 확인
-            return visited[ti][tj] - 2
-        for di, dj in [[0,1],[1,0],[0,-1],[-1,0]]:
-            wi,wj = ti + di, tj + dj
-            if 0 <= wi < N and 0 <= wj < N and maze[wi][wj] != 1 and visited[wi][wj] == 0:
-                q.append([wi,wj])
-                visited[wi][wj] = visited[ti][tj] + 1
-
-
 T = int(input())
+
+def bfs(N,maze,start):
+    # 시작점 
+    x, y = start[0],start[1]
+    queue = [(x,y,0)]
+    
+    visited = [[False] * N for _ in range(N)]
+    visited[x][y] = True
+    front = 0
+    
+    dx = [1,-1,0,0]
+    dy = [0,0,1,-1]
+    
+    while front < len(queue):
+        x,y,dist = queue[front]
+        front += 1
+        
+        for d in range(4):
+            nx,ny = x + dx[d], y+dy[d]
+            if 0 <= nx < N and 0 <= ny < N and visited[nx][ny] == 0:
+                if maze[nx][ny] == 0:
+                    visited[nx][ny] = True
+                    queue.append((nx,ny,dist+1))
+                elif maze[nx][ny] == 3:
+                    return dist
+             
+
 for t in range(1,T+1):
     N = int(input())
-    maze = [list(map(int,input())) for _ in range(N)]
-    sti, stj = find_start(N)
-    ans = bfs(sti,stj,N)
-    if ans == None:
-        print(f"#{t} 0")
-    else: print(f"#{t} {ans}")
+    arr = [list(map(int,input().strip())) for _ in range(N)]
+    
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] == 2:
+                start = [i,j]
+	
+    answer = bfs(N,arr,start)
+    if answer == None:
+        answer = 0
+    print(f"#{t} {answer}")
+    
+    
